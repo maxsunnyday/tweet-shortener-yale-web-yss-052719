d7b7d3847ab59c036cf189f965354049e78753f9
Max Sun <max.sun@yale.edu>
2019-05-17 20:24:32 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -29,7 +29,7 @@ end
 
 def bulk_tweet_shortener(tweets)
   short_tweets = []
-  tweets.each do |tweet|
-    short_tweets << puts word_substituter(tweet)
+  tweets.collect do |tweet|
+    puts word_substituter(tweet)
   end
 end
